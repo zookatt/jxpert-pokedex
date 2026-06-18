846e2440629556f80800947b70281402973a351b
Ekaterina Zotova <139338863+zookatt@users.noreply.github.com>
2026-06-18 11:01:40 +0200

    test: configure vitest test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
-import svgrPlugin from "vite-plugin-svgr";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgrPlugin()],
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    globals: true,
+  },
 });
