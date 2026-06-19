3e960f0f42677460cd11a6159a08c62381ea36ed
Ekaterina Zotova <139338863+zookatt@users.noreply.github.com>
2026-06-19 12:22:43 +0200

    feat: add header navigation links

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,12 +1,26 @@
 import { Link } from "react-router-dom";
 import pokeball from "../../assets/pokeball.svg";
+import stars from "../../assets/stars.svg";
 
 export function Header() {
   return (
     <header className="header">
       <Link to="/" className="header__brand" aria-label="Go to Pokédex home">
-        <img src={pokeball} alt="" className="header__logo" />
-        <p className="header__title">Pokédex</p>
+        <img src={pokeball} alt="Pokédex logo" className="header__logo" />
+        <span className="header__title">Pokédex</span>
+      </Link>
+
+      <Link
+        to="/favorites"
+        className="header__dreamteam"
+        aria-label="Go to Pokédex favorites"
+      >
+        <img
+          src={stars}
+          alt="Pokédex logo favorites"
+          className="header__dreamteam__icon"
+        />
+        <span className="header__dreamteam__title">Dream team</span>
       </Link>
     </header>
   );
