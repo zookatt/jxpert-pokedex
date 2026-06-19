94be24be2935647b48a7c3be1366e020f7d6ad40
Ekaterina Zotova <139338863+zookatt@users.noreply.github.com>
2026-06-19 12:07:11 +0200

    feat: link header brand to home

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import pokeball from "../../assets/pokeball.svg";
 
 export function Header() {
   return (
     <header className="header">
-      <img src={pokeball} alt="" className="header__logo" />
-      <p className="header__title">Pokédex</p>
+      <Link to="/" className="header__brand" aria-label="Go to Pokédex home">
+        <img src={pokeball} alt="" className="header__logo" />
+        <p className="header__title">Pokédex</p>
+      </Link>
     </header>
   );
 }
